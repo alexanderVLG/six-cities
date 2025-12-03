fe6a6504221d0a1cc5c371596c22854dcd97483f
Alexander Kovalev <a.kovalev199@yandex.ru>
2025-12-03 11:29:21 +0500

    Добавляет компонент приватного маршрута.

--- src/const.ts
+++ src/const.ts
@@ -1,9 +1,16 @@
 export const Hotels = {
   hotelsNumber: 312
 };
+
 export enum AppRoute {
   Root = '/',
   Favorites = '/favorites',
   Login = '/login',
   Offer = '/offer'
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
 }
