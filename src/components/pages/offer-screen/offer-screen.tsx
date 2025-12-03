da031f05ad6a1de822b24eda2da57395bd6c3677
Alexander Kovalev <a.kovalev199@yandex.ru>
2025-12-03 10:07:14 +0500

    Убирает повторяющиеся элементы offer-screen

--- src/components/pages/offer-screen/offer-screen.tsx
+++ src/components/pages/offer-screen/offer-screen.tsx
@@ -1,5 +1,7 @@
 import Header from '../../layout/header';
 
+const offerInsideList = ['Wi-fi', 'Towels', 'Heating', 'Dishwasher', 'Kitchen', 'Soap'];
+
 function OfferScreen() {
   return(
     <div className="page">
@@ -70,36 +72,9 @@ function OfferScreen() {
               <div className="offer__inside">
                 <h2 className="offer__inside-title">What&apos;s inside</h2>
                 <ul className="offer__inside-list">
-                  <li className="offer__inside-item">
-                    Wi-Fi
-                  </li>
-                  <li className="offer__inside-item">
-                    Washing machine
-                  </li>
-                  <li className="offer__inside-item">
-                    Towels
-                  </li>
-                  <li className="offer__inside-item">
-                    Heating
-                  </li>
-                  <li className="offer__inside-item">
-                    Coffee machine
-                  </li>
-                  <li className="offer__inside-item">
-                    Baby seat
-                  </li>
-                  <li className="offer__inside-item">
-                    Kitchen
-                  </li>
-                  <li className="offer__inside-item">
-                    Dishwasher
-                  </li>
-                  <li className="offer__inside-item">
-                    Cabel TV
-                  </li>
-                  <li className="offer__inside-item">
-                    Fridge
-                  </li>
+                  {offerInsideList.map(
+                    (item) => <li className="offer__inside-item" key={item}>{item}</li>
+                  )}
                 </ul>
               </div>
               <div className="offer__host">
