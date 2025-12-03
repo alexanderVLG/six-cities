c4bca036986cb97bc7605dedeca2c2c721bc2cc6
Alexander Kovalev <a.kovalev199@yandex.ru>
2025-12-03 12:20:10 +0500

    Заменяет Route на PrivateRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,10 +1,11 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { AppRoute, AuthorizationStatus} from '../../const';
 import MainScreen from '../pages/main-screen/main-screen';
 import FavoritesScreen from '../pages/favorites-screen/favorites-screen';
 import LoginScreen from '../pages/login-screen/login-screen';
 import OfferScreen from '../pages/offer-screen/offer-screen';
 import NotFoundScreen from '../pages/not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppHotelsProps = {
   hotelsNumber: number;
@@ -14,11 +15,32 @@ function App({hotelsNumber}: AppHotelsProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Root} element={<MainScreen hotelsNumber={hotelsNumber}/>}/>
-        <Route path={AppRoute.Favorites} element={<FavoritesScreen />} />
-        <Route path={AppRoute.Login} element={<LoginScreen />} />
-        <Route path={AppRoute.Offer} element={<OfferScreen />} />
-        <Route path="*" element={<NotFoundScreen />} />
+        <Route
+          path={AppRoute.Root}
+          element={<MainScreen hotelsNumber={hotelsNumber}/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute
+              authorizationStatus={AuthorizationStatus.NoAuth}
+            >
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
       </Routes>
     </BrowserRouter>
   );
