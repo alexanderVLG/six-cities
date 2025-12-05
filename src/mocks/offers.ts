3f92739936cd14490132d54ede3193dfddbd9d93
Alexander Kovalev <a.kovalev199@yandex.ru>
2025-12-05 16:48:28 +0500

    Добавляет моковые данные с офферами

---
+++ src/mocks/offers.ts
@@ -0,0 +1,94 @@
+export const offersData = [
+  {
+    'id': 'cce1e9d7-1f08-40ea-ae2a-0d30c9f2241d',
+    'title': 'Perfectly located Castro',
+    'type': 'house',
+    'price': 527,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/9.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.868610000000004,
+      'longitude': 2.342499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': true,
+    'rating': 2.7
+  },
+  {
+    'id': '5716152d-8856-4580-b9ac-2ee5d9a1b225',
+    'title': 'The Joshua Tree House',
+    'type': 'hotel',
+    'price': 173,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/3.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.858610000000006,
+      'longitude': 2.330499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 1.9
+  },
+  {
+    'id': '35ca2920-9948-48d7-849a-35f4e71aa7a0',
+    'title': 'Canal View Prinsengracht',
+    'type': 'room',
+    'price': 236,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/9.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.834610000000005,
+      'longitude': 2.335499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 4.9
+  },
+  {
+    'id': 'efd45968-c937-4ef9-8b10-3cf7dcbca863',
+    'title': 'Nice, cozy, warm big bed apartment',
+    'type': 'room',
+    'price': 217,
+    'previewImage': 'https://15.design.htmlacademy.pro/static/hotel/19.jpg',
+    'city': {
+      'name': 'Paris',
+      'location': {
+        'latitude': 48.85661,
+        'longitude': 2.351499,
+        'zoom': 13
+      }
+    },
+    'location': {
+      'latitude': 48.85761,
+      'longitude': 2.358499,
+      'zoom': 16
+    },
+    'isFavorite': false,
+    'isPremium': false,
+    'rating': 1.8
+  }
+];
