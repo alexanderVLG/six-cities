67ec447005d61aeee89621302ae7055b67b2a7e5
Alexander Kovalev <a.kovalev199@yandex.ru>
2026-01-04 13:15:58 +0500

    Меняет структуру типов

---
+++ src/types.ts
@@ -0,0 +1,46 @@
+export type LocationType = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+export type CityType = {
+  name: string;
+  location: LocationType;
+}
+
+export type PlaceOfferType = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  previewImage?: string;
+  city: CityType;
+  location: LocationType;
+  isFavorite: boolean;
+  isPremium: boolean;
+  rating: number;
+}
+
+export type OfferType = PlaceOfferType & {
+  description: string;
+  bedrooms: number;
+  goods: string[];
+  host: User;
+  images: string[];
+  maxAdults: number;
+};
+
+export type ReviewType = {
+  id: string;
+  date: string;
+  user: User;
+  comment: string;
+  rating: number;
+};
+
+export type User = {
+  name: string;
+  avatarUrl: string;
+  isPro: boolean;
+};
