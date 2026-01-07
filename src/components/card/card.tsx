1b7536654544784df78fd5ed2c698e896ac0b0e6
Alexander Kovalev <a.kovalev199@yandex.ru>
2026-01-07 11:44:24 +0500

    Переделывает компонент Card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,91 +1,62 @@
 import { Link } from 'react-router-dom';
-import { OfferType, PlaceOfferType } from '../../types';
+import { PlaceOfferType } from '../../types';
 import { AppRoute } from '../../const';
 
-type CardVariantProps = 'cities' | 'favorites';
 
 type CardProps = {
-  data: OfferType | PlaceOfferType;
-  variant?: CardVariantProps;
+  placeOffer: PlaceOfferType;
+  classNameCard: string;
+  imageWidth: string;
+  imageHeight: string;
   onMouseEnter?: () => void;
   onMouseLeave?: () => void;
-  isActive?: boolean;
 }
 
+const PremiumMark = (): JSX.Element => (
+  <div className="place-card__mark">
+    <span>Premium</span>
+  </div>
+);
+
 function Card({
-  data,
-  variant,
+  placeOffer,
+  classNameCard,
+  imageWidth,
+  imageHeight,
   onMouseEnter,
-  onMouseLeave,
-  isActive
+  onMouseLeave
 }: CardProps): JSX.Element {
 
-  const getContainerClasses = () => {
-    switch (variant) {
-      case 'favorites':
-        return 'favorites__card place-card';
-      case 'cities':
-      default:
-        return `cities__card place-card ${isActive ? 'active' : null}`;
-    }
-  };
+  const {title, previewImage, price, isFavorite, rating, type} = placeOffer;
+  const favoriteClass = isFavorite
+    ? 'place-card__bookmark-button place-card__bookmark-button--active button'
+    : 'place-card__bookmark-button button';
 
-  const getImageWrapperClasses = () => {
-    switch (variant) {
-      case 'favorites':
-        return 'favorites__image-wrapper place-card__image-wrapper';
-      case 'cities':
-      default:
-        return 'cities__image-wrapper place-card__image-wrapper';
-    }
+  const CardStyle = {
+    FOR_ARTICLE: `${classNameCard }__card place-card`,
+    FOR_DIV: `${classNameCard }__image-wrapper place-card__image-wrapper`
   };
-
-  const getInfoClasses = () => {
-    if (variant === 'favorites') {
-      return 'favorites__card-info place-card__info';
-    }
-    return 'place-card__info';
-  };
-
-  const getImageSize = () => {
-    switch (variant) {
-      case 'favorites':
-        return { width: 150, height: 110 };
-      default:
-        return { width: 260, height: 200 };
-    }
-  };
-
-  const {width, height} = getImageSize();
 
   return (
     <article
-      key={data.id}
-      className={getContainerClasses()}
+      className={CardStyle.FOR_ARTICLE}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
-      {data.isPremium ? (
-        <div className="place-card__mark">
-          <span>Premium</span>
-        </div>
-      ) : null}
-      <div className={getImageWrapperClasses()}>
+      {isFavorite ? <PremiumMark /> : ''}
+
+      <div className={CardStyle.FOR_DIV}>
         <a href="#">
-          <img className="place-card__image" src={data.previewImage} width={width} height={height} alt="Place image" />
+          <img className="place-card__image" src={previewImage} width={imageWidth} height={imageHeight} alt="Place image" />
         </a>
       </div>
-      <div className={getInfoClasses()}>
+      <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{data.price}</b>
+            <b className="place-card__price-value">&euro;{price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className={`place-card__bookmark-button button ${
-            data.isFavorite ? 'place-card__bookmark-button--active' : ''
-          }`}
-          type="button"
-          >
+          <button className={favoriteClass} type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
@@ -94,14 +65,14 @@ function Card({
         </div>
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{width: `${(data.rating / 5) * 100}%`}}></span>
+            <span style={{width: `${(rating / 5) * 100}%`}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <Link to={AppRoute.Offer}>{data.title}</Link>
+          <Link to={AppRoute.Offer}>{title}</Link>
         </h2>
-        <p className="place-card__type">{data.type}</p>
+        <p className="place-card__type">{type}</p>
       </div>
     </article>);
 }
