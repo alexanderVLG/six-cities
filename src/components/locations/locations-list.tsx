2123157e90c9832eaa7a7ee6f2134e4432950b1d
Alexander Kovalev <a.kovalev199@yandex.ru>
2026-02-01 15:37:00 +0500

    Добавляет компонент список локаций

---
+++ src/components/locations/locations-list.tsx
@@ -0,0 +1,39 @@
+
+type LocationsProps = {
+  cities: string[];
+  onCity: (currentCity:string) => void;
+  currentCity: string;
+}
+
+type LocationItemProps = {
+  city: string;
+  onCity: (currentCity:string) => void;
+  currentCity: string;
+}
+
+const LocationsItem = ({city, onCity, currentCity}: LocationItemProps): JSX.Element => {
+
+  const classCityItem = city === currentCity
+    ? 'locations__item-link tabs__item tabs__item--active'
+    : 'locations__item-link tabs__item';
+
+  return (
+    <li onClick={() => onCity(city)} className="locations__item">
+      <a className={classCityItem} href="#">
+        <span>{city}</span>
+      </a>
+    </li>
+  );
+};
+
+const LocationsList = ({cities, onCity, currentCity}: LocationsProps): JSX.Element => (
+  <div className="tabs">
+    <section className="locations container">
+      <ul className="locations__list tabs__list">
+        {cities.map((city) => <LocationsItem key={city} city={city} onCity={onCity} currentCity={currentCity}/>)}
+      </ul>
+    </section>
+  </div>
+);
+
+export default LocationsList;
