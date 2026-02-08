c54f2e61d93644aaf9a6465a86b18d844107382d
Alexander Kovalev <a.kovalev199@yandex.ru>
2026-02-08 15:52:24 +0500

    Подключает leaflet к компоненту.

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,4 +1,27 @@
-function Map() {
+import { Icon, Marker, layerGroup } from 'leaflet';
+import { City, PlaceOfferType } from '../../types';
+import { URL_MARKER_DEFAULT, URL_MARKER_CURRENT } from '../../const';
+import 'leaflet/dist/leaflet.css';
+
+type MapProps = {
+  city: City;
+  placeOfferType: PlaceOfferType[];
+}
+
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+function Map(props: MapProps):JSX.Element {
+  const {city, placeOfferType} = props;
   return (
     <section className="cities__map map"></section>
   );
--- src/const.ts
+++ src/const.ts
@@ -23,3 +23,9 @@ export const CommentLentgh = {
   MIN: 50,
   MAX: 300
 };
+
+export const URL_MARKER_DEFAULT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+
+export const URL_MARKER_CURRENT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
