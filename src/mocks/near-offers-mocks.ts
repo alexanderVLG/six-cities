ed04545edd3baa9685f40383f6dd8a02dc7c740e
Alexander Kovalev <a.kovalev199@yandex.ru>
2026-02-16 06:51:13 +0500

    меняет данные в моке near-offers

--- src/mocks/near-offers-mocks.ts
+++ src/mocks/near-offers-mocks.ts
@@ -15,14 +15,14 @@ export const nearOffers = [{
   city: {
     name: 'Amsterdam',
     location: {
-      latitude: 48.85661,
-      longitude: 2.351499,
+      latitude: 52.37454,
+      longitude: 4.897976,
       zoom: 13
     }
   },
   location: {
-    latitude: 48.85761,
-    longitude: 2.358499,
+    latitude: 52.367540000000005,
+    longitude: 4.873976,
     zoom: 16
   },
   goods: [
@@ -61,14 +61,14 @@ export const nearOffers = [{
   city: {
     name: 'Amsterdam',
     location: {
-      latitude: 48.85661,
-      longitude: 2.351499,
+      latitude: 52.3745,
+      longitude: 4.897976,
       zoom: 13
     }
   },
   location: {
-    latitude: 48.85761,
-    longitude: 2.358499,
+    latitude: 52.361540000000005,
+    longitude: 4.893976,
     zoom: 16
   },
   goods: [
@@ -107,14 +107,14 @@ export const nearOffers = [{
   city: {
     name: 'Amsterdam',
     location: {
-      latitude: 48.85661,
-      longitude: 2.351499,
+      latitude: 52.37454,
+      longitude: 4.897976,
       zoom: 13
     }
   },
   location: {
-    latitude: 48.85761,
-    longitude: 2.358499,
+    latitude: 52.35754,
+    longitude: 4.9179759999999995,
     zoom: 16
   },
   goods: [
@@ -135,51 +135,4 @@ export const nearOffers = [{
   rating: 3.2,
   bedrooms: 5,
   maxAdults: 2
-},
-{
-  id: '9c21c854-fb07-4822-ac05-cb2da82ea58699',
-  title: 'Loft Studio in the Central Area',
-  description: 'I am happy to welcome you to my apartment in the city center! Three words: location, cosy and chic!',
-  type: 'house',
-  price: 248,
-  images: [
-    'https://16.design.htmlacademy.pro/static/hotel/11.jpg',
-    'https://16.design.htmlacademy.pro/static/hotel/18.jpg',
-    'https://16.design.htmlacademy.pro/static/hotel/20.jpg',
-    'https://16.design.htmlacademy.pro/static/hotel/10.jpg',
-    'https://16.design.htmlacademy.pro/static/hotel/6.jpg',
-    'https://16.design.htmlacademy.pro/static/hotel/7.jpg'
-  ],
-  city: {
-    name: 'Amsterdam',
-    location: {
-      latitude: 48.85661,
-      longitude: 2.351499,
-      zoom: 13
-    }
-  },
-  location: {
-    latitude: 48.85761,
-    longitude: 2.358499,
-    zoom: 16
-  },
-  goods: [
-    'Air conditioning',
-    'Fridge',
-    'Washing machine',
-    'Towels',
-    'Laptop friendly workspace',
-    'Coffee machine'
-  ],
-  host: {
-    isPro: true,
-    name: 'Angelina',
-    avatarUrl: 'https://16.design.htmlacademy.pro/static/host/avatar-angelina.jpg'
-  },
-  isPremium: false,
-  isFavorite: true,
-  rating: 3.2,
-  bedrooms: 5,
-  maxAdults: 2
-}
-];
+}];
