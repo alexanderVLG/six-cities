9e478c78ac6d289dba23c4bad751504031be2668
Alexander Kovalev <a.kovalev199@yandex.ru>
2026-04-10 08:39:19 +0500

    Меняет адрес бекенда.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, {AxiosInstance} from 'axios';
 
-const BACKEND_URL = 'https://15.design.htmlacademy.pro/six-cities';
+const BACKEND_URL = 'https://15.design.htmlacademy.pro/spec/project/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
