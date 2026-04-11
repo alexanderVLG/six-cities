e659e9a4b68d82fc3f85bc78927aac1e14b27f5f
Alexander Kovalev <a.kovalev199@yandex.ru>
2026-04-11 08:37:56 +0500

    Добавляет х-токен в заголовок в конфигурации.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-import axios, {AxiosInstance} from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
 
 const BACKEND_URL = 'https://15.design.htmlacademy.pro/spec/project/six-cities';
 const REQUEST_TIMEOUT = 5000;
@@ -10,7 +10,15 @@ export const createAPI = (): AxiosInstance => {
   });
 
   api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
 
+      if(token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
   );
 
   return api;
